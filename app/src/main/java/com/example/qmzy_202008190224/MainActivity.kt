765aba2d09920f47e6801fa6fa85475575b757e7
kzr <kzr002211@gmail.com>
2022-12-10 16:22:28 +0800

    First commit.

---
+++ app/src/main/java/com/example/qmzy_202008190224/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.qmzy_202008190224
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
